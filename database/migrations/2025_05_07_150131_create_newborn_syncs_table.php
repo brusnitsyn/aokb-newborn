a1ff1803ab0096fb2e4612d1b7a63630abc5d797
brusnitsyn <brusnitsyn@gmail.com>
2025-05-12 13:37:22 +0900

    Вернул временные метки

--- database/migrations/2025_05_07_150131_create_newborn_syncs_table.php
+++ database/migrations/2025_05_07_150131_create_newborn_syncs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('newborn_syncs', function (Blueprint $table) {
             $table->id();
             $table->dateTime('last_bd');
+            $table->timestamps();
         });
     }
 
