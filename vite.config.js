43845a0c0cafc5f786b945b3cb6275e31cb2581f
Andrew Brusnitsyn <42877650+brusnitsyn@users.noreply.github.com>
2026-06-10 18:24:43 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,14 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
         tailwindcss(),
     ],
 });
